d01b4509ac3c9a3c9b0650784ee7bc5a85ef2ffd
Developer <developer@example.com>
2026-04-16 17:14:22 +0300

    chore: auto-commit 1 file(s) changed

--- tmp_fix_vrd.ps1
+++
@@ -1,5 +0,0 @@
-$vrdPath = "C:\inetpub\wwwroot\DEV_ATERLETSKIY_39144_MFM\default.vrd"
-$c = Get-Content $vrdPath -Raw
-$c = $c.Replace('<standardOdata enable="false"', "<httpServices publishExtensionsByDefault=`"true`"/>`r`n`t<standardOdata enable=`"false`"")
-Set-Content $vrdPath -Value $c -Encoding UTF8
-Write-Host "DONE"
